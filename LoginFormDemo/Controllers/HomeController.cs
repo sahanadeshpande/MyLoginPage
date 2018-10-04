ea964f96a628fddd79d9155eaa0009217f3c5a28
sahanadeshpande <43597700+sahanadeshpande@users.noreply.github.com>
2018-10-04 12:19:09 +0530

    Add files via upload

---
+++ LoginFormDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using LoginFormDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginFormDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserLoginModel objUserLogin)
+        {
+            if (objUserLogin.UserName == "admin" && objUserLogin.Password == "admin")
+                return RedirectToAction("WelcomePage");
+            else
+            {
+                objUserLogin.Message = "Invalid UserName/Password";
+                return View(objUserLogin);
+            }
+
+        }
+
+        public ActionResult WelcomePage()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LoginFormDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginFormDemo.MvcApplication" Language="C#" %>
